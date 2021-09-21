3c21cb2c4677d9f2c8dd0a5f77922cf0a85ea7d5
Jacob-s-unger <46698823+Jacob-s-unger@users.noreply.github.com>
2021-09-20 20:41:10 -0600

    Creating a merge conflict!

--- BranchFiles.py
+++ BranchFiles.py
@@ -1,1 +1,3 @@
 print("This is the test file that I am sending to the Branch called testing-new-files. Hope this works!")
+print("This might create a merge conflict")
+print("If it does create a merge conflict, Ill need to screen cap and submit with the assignment")
